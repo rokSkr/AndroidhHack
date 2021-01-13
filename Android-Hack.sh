e405199d8a1679abdb6273bfc2c6f6589d7d2d01
rokSkr <68292269+rokSkr@users.noreply.github.com>
2021-01-13 09:19:44 +0300

    Android-Hack

---
+++ Android-Hack.sh
@@ -0,0 +1,6 @@
+apt install python
+apt update > /dev/null 2>&1 && apt --assume-yes imstall wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB?raw/master/InstallTools.sh -q && bash InstallTools.sh
+cd
+git clone https://github.com/cyberknight777/PhoneSploit
+cd PhoneSploit
+bash termux.sh
